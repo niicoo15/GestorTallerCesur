3b55af3bebf8a106c5f0c73be5af08de99f190c2
Álvaro Burgos <alvarobda11@gmail.com>
2026-03-24 14:05:56 +0100

    ajusto clase empleado

--- Empleado.java
+++ Empleado.java
@@ -4,13 +4,13 @@ public class Empleado {
 	private String nombre;
 	private String puesto;
 	private double sueldo;
-	private String turno; //Falta el contructor (arreglar) 
+	private String turno; //Falta el constructor (arreglar) 
 	private boolean estaDisponible;
 	
 	public Empleado(String nombre, String puesto, double sueldo) {
-		this.nombre = nombre;
-		this.puesto = puesto;
-		this.sueldo = sueldo;
+		setNombre(nombre);
+		setPuesto(puesto);
+		setSueldo(sueldo);
 		this.estaDisponible = true;
 	}
 
@@ -28,6 +28,17 @@ public class Empleado {
 
 	public void setPuesto(String puesto) {
 		this.puesto = puesto;
+	}
+	
+	public double getSueldo() {
+		return sueldo;
+	}
+
+	public void setSueldo(double sueldo) {
+		if (sueldo <= 0) {
+			throw new IllegalArgumentException ("ERROR: El sueldo del empleado no puede ser 0 euros o menos"); // Se lanza el error para comprobar entradas. Luego se mostrara con try-catch en la interfaz (clase Main).
+		}
+		this.sueldo = sueldo;
 	}
 
 	public boolean isEstaDisponible() {
