7e2f24904578043393ea74576fd74b1804161462
Carlos <carlosdavid.s547379@cesurformacion.com>
2026-03-25 11:32:34 +0100

    guardo mis cambios

--- Factura.java
+++ Factura.java
@@ -5,5 +5,9 @@ import java.util.ArrayList;
 public class Factura {
 	
 	private int id;
-	private ArrayList<Item> listaElementos;
+	private Cita cita;
+	private Cliente cliente;
+	private ArrayList<Factura> listafacturas;
+	
+	
 }
--- Item.java
+++ Item.java
@@ -1,5 +1,10 @@
 package GestorTallerCesur;
 
-public class Item {
+import java.util.ArrayList;
 
+public class Item {
+	private String nombre;
+	private String marca;
+	private int precio;
+	private ArrayList<Item> listaElementos;
 }
